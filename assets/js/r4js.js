4b0581f4667f18cb3e8e01057ef9cc3d6bc9bc1b
hat_lover <jobin25051999@gmail.com>
2020-09-25 22:58:22 +0530

    front design

--- assets/js/r4js.js
+++ assets/js/r4js.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-    var imageFile = ["main.png", "main1.png", "main2.png"];
+    var imageFile = ["b.png","c.png","a.png"];
     var currentIndex = 0;
 
     setInterval(function () {
         if (currentIndex == imageFile.length) {
             currentIndex = 0;
         }
-        $(".trans-image").css('background-image', 'url("assets/img/' + imageFile[currentIndex++] + '")')
+        $(".trans-image").css('background-image', 'url("assets/img/background/' + imageFile[currentIndex++] + '")')
             .css('background-position','top right')
-            .css('background-size','cover');
-       }, 3000);
+            .css('background-size','cover').animate({'opacity':'.8'},1000).animate({'opacity':'1'},1500);
+       }, 6000);
 
 });
-console.log("fgfgfg");
+console.log("fgfgfg"); 
